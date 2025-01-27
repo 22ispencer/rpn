d0305325f83ef7f4da0a8ad235fdfe25fc79fd16
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-01-27 12:57:10 -0600

    notcurses hello world

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
+#include <notcurses/notcurses.h>
 #include <stdio.h>
+#include <unistd.h>
 
 int main() {
-  printf("Hello, World");
+  struct notcurses *nc = notcurses_init(NULL, NULL);
+  if (nc == NULL) {
+    printf("unable to initialize notcurses");
+    return 1;
+  }
+
+  unsigned int y;
+
+  struct ncplane *stdplane = notcurses_stddim_yx(nc, &y, NULL);
+
+  char message[] = "Hello, World!";
+
+  ncplane_putstr_aligned(stdplane, y / 2, NCALIGN_CENTER, message);
+
+  notcurses_render(nc);
+  sleep(5);
+
+  notcurses_stop(nc);
   return 0;
 }
